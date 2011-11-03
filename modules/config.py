27852b97ec5da3e1cd3eeed7aa1b2fd5df8d2e5e
Patrick <patrick@eefy.net>
2011-11-02 22:32:53 -0500

    More configuration error checking

--- modules/config.py
+++ modules/config.py
@@ -16,6 +16,21 @@ if not config.has_section("default"):
     sys.exit(1)
 
 host = config.get('default', 'host')
+if not host:
+    sys.stderr.write("No default host defined!\n")
+    sys.exit(1)
+
 port = config.get('default', 'port')
+if not port:
+    sys.stderr.write("No default port defined!\n")
+    sys.exit(1)
+
 replicas = config.get('default', 'replicas')
+if not replicas:
+    sys.stderr.write("No default replicas defined!\n")
+    sys.exit(1)
+
 shards = config.get('default', 'shards')
+if not shards:
+    sys.stderr.write("No default shards defined!\n")
+    sys.exit(1)
