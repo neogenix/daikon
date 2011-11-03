bd1fdcf974f4d4bada139128db50105a8c766285
Patrick <patrick@eefy.net>
2011-11-03 14:37:05 -0500

    * Updated daikon to include --cluster in most options * Refactored modules/config.py to include configuration function, and called from daikon executable

--- modules/config.py
+++ modules/config.py
@@ -2,33 +2,35 @@ import ConfigParser
 import sys
 import os.path
 
-config = ConfigParser.ConfigParser()
 
-if not config.read(['/etc/daikon/daikon.conf',
-        os.path.expanduser('~/.daikon.conf'), 'daikon.conf']):
-    sys.stderr.write("No config file found!\n")
-    sys.exit(1)
+def configuration(cluster='default'):
+    config = ConfigParser.ConfigParser()
 
-if not config.has_section("default"):
-    sys.stderr.write("No default config section defined!\n")
-    sys.exit(1)
+    if not config.read(['/etc/daikon/daikon.conf',
+                os.path.expanduser('~/.daikon.conf'), 'daikon.conf']):
+        sys.stderr.write("No config file found!\n")
+        sys.exit(1)
 
-host = config.get('default', 'host')
-if not host:
-    sys.stderr.write("No default host defined!\n")
-    sys.exit(1)
+    if not config.has_section(cluster):
+        sys.stderr.write("No cluster config section defined for this cluster!\n")
+        sys.exit(1)
 
-port = config.get('default', 'port')
-if not port:
-    sys.stderr.write("No default port defined!\n")
-    sys.exit(1)
+    host = config.get(cluster, 'host')
+    if not host:
+        sys.stderr.write("No default host defined!\n")
+        sys.exit(1)
 
-replicas = config.get('default', 'replicas')
-if not replicas:
-    sys.stderr.write("No default replicas defined!\n")
-    sys.exit(1)
+    port = config.get(cluster, 'port')
+    if not port:
+        sys.stderr.write("No default port defined!\n")
+        sys.exit(1)
 
-shards = config.get('default', 'shards')
-if not shards:
-    sys.stderr.write("No default shards defined!\n")
-    sys.exit(1)
+    replicas = config.get(cluster, 'replicas')
+    if not replicas:
+        sys.stderr.write("No default replicas defined!\n")
+        sys.exit(1)
+
+    shards = config.get(cluster, 'shards')
+    if not shards:
+        sys.stderr.write("No default shards defined!\n")
+        sys.exit(1)
