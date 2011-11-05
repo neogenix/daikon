6ac5bc57ae8744c1fdaabbc8a4177e55c97bd0f2
Patrick <patrick@eefy.net>
2011-11-05 00:21:46 -0500

    Reworked stuff because I'm an idiot.

--- modules/es_config.py
+++ modules/es_config.py
@@ -16,26 +16,26 @@ def configuration(cluster):
         sys.stderr.write("No cparser file found!\n")
         sys.exit(1)
 
-    if not cparser.has_section(config.cluster):
+    if not cparser.has_section(config["cluster"]):
         sys.stderr.write("No cluster section defined for this cluster!\n")
         sys.exit(1)
 
     config["host"] = cparser.get(cluster, 'host')
-    if not config.host:
+    if not config["host"]:
         sys.stderr.write("No default host defined!\n")
         sys.exit(1)
 
     config["port"] = cparser.get(cluster, 'port')
-    if not config.port:
+    if not config["port"]:
         sys.stderr.write("No default port defined!\n")
         sys.exit(1)
 
     config["replicas"] = cparser.get(cluster, 'replicas')
-    if not config.replicas:
+    if not config["replicas"]:
         sys.stderr.write("No default replicas defined!\n")
         sys.exit(1)
 
     config["shards"] = cparser.get(cluster, 'shards')
-    if not config.shards:
+    if not config["shards"]:
         sys.stderr.write("No default shards defined!\n")
         sys.exit(1)
