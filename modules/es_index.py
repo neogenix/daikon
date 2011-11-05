bd84b410a2e1c10f7d9936d27fa59df9685a3dba
Patrick <patrick@eefy.net>
2011-11-05 17:45:55 -0500

    Error handling for es_index.py and index_create

--- modules/es_index.py
+++ modules/es_index.py
@@ -5,20 +5,32 @@ import sys
 
 def index_create(host, port, indexname, shards, replicas):
 
-    data = json.dumps([{"index" : { "number_of_shards" : shards,
+    data_out = json.dumps([{"index" : { "number_of_shards" : shards,
         "number_of_replicas" : replicas } }], sort_keys=True, indent=4)
-    request = requests.post('http://' + host + ':' + port + '/' + indexname,
-            data=data)
 
-    if request.raise_for_status() is not None:
-        print request.raise_for_status()
-        sys.exit[1]
+    request = requests.post('http://' + host + ':' + port + '/' + indexname,
+            data=data_out)
+
+    data_in = json.loads(request.content)
 
     if not request.status_code == requests.codes.ok:
-        print request.content
-        print request.status_code
-        print request.raise_for_status()
+        print
+        print 'ERROR - Error Creating Index'
+        print
+        print ' Status Code:', request.status_code
+        print ' Error Message:', data_in[u'error']
+        print
+        print ' Host:', host
+        print ' Replicas:', replicas
+        print ' Shards:', shards
+        print
     else:
-        print 'Index "' + indexname + '" Created Successfully'
+        print
+        print 'SUCCESS - Index "' + indexname + '" Created Successfully'
+        print
+        print ' Host:', host
+        print ' Replicas:', replicas
+        print ' Shards:', shards
+        print
 
 #def index_delete(host, indexname):
