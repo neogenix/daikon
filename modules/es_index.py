132aa60ab3caee36df2c5d7b49f1020b72ebf71c
Patrick <patrick@eefy.net>
2011-11-04 23:34:23 -0500

    First work towrads plumbing, and functions for index work

--- modules/es_index.py
+++ modules/es_index.py
@@ -1,8 +1,11 @@
-# Function(s): Access to and from ES for indexes, etc...
+import requests
+import json
 
 def index_create(host, indexname, shards, replicas):
+    data = json.dumps([{"index" : { "number_of_shards" : shards,
+        "number_of_replicas": replicas } }], sort_keys = True, indent = 4)
+    request = requests.post('http://' + host + '/' + indexname)
+    print data
 
-
-def index_delete(host, indexname):
-
+#def index_delete(host, indexname):
 
