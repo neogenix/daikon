808a957addc8b5a35d91582adbcea5a75e38b281
Patrick <patrick@eefy.net>
2011-11-07 00:34:14 -0600

    Updated es_cluster to supply index state from health. This should close #32

--- daikon/modules/es_cluster.py
+++ daikon/modules/es_cluster.py
@@ -5,42 +5,58 @@ import sys
 
 def cluster_status(cluster, host, port, extended):
     try:
-        request = requests.get('http://' + host + ':' + port +
-                '/_cluster/state')
-        if request.error is not None:
-            print 'ERROR: Fetching Cluster Status : "' + cluster + '" -', request.error
+        request_health = requests.get('http://' + host + ':' + port +
+                '/_cluster/health?level=indices')
+        if request_health.error is not None:
+            print 'ERROR: Fetching Cluster Status : "' + cluster + '" -', request_health.error
             sys.exit(1)
         else:
-            request.raise_for_status()
+            request_health.raise_for_status()
+
+        request_state = requests.get('http://' + host + ':' + port +
+                '/_cluster/state')
+        if request_state.error is not None:
+            print 'ERROR: Fetching Cluster Status : "' + cluster + '" -', request_state.error
+            sys.exit(1)
+        else:
+            request_state.raise_for_status()
+    except request_health.RequestException, e:
+        print 'ERROR: Fetching Cluster Status : "' + cluster + '" -',  e
+        sys.exit(1)
     except requests.RequestException, e:
         print 'ERROR: Fetching Cluster Status : "' + cluster + '" -',  e
         sys.exit(1)
     else:
         print 'SUCCESS: Fetching Cluster Status : "' + cluster + '"\n'
-        data_result = json.loads(request.content)
-        master_node = data_result[u'master_node']
+        data_result_state = json.loads(request_state.content)
+        data_result_health = json.loads(request_health.content)[u'indices']
+        master_node = data_result_state[u'master_node']
 
         print '\t Information:'
-        print '\t\t Cluster Name:', data_result[u'cluster_name']
-        print '\t\t Master Node:', data_result[u'master_node']
+        print '\t\t Cluster Name:', data_result_state[u'cluster_name']
+        print '\t\t Master Node:', data_result_state[u'master_node']
         if extended:
-            print '\t\t\t Name:', data_result[u'nodes'][master_node][u'name']
-            print '\t\t\t Transport Address:', data_result[u'nodes'][master_node][u'transport_address']
+            print '\t\t\t Name:', data_result_state[u'nodes'][master_node][u'name']
+            print '\t\t\t Transport Address:', data_result_state[u'nodes'][master_node][u'transport_address']
 
         print '\t Indices:'
-        for index in data_result[u'metadata'][u'indices']:
+        for index in data_result_state[u'metadata'][u'indices']:
             print '\t\t Name:', index
             if extended:
-                print '\t\t\t State:', data_result[u'metadata'][u'indices'][index][u'state']
-                print '\t\t\t Replicas:', data_result[u'metadata'][u'indices'][index][u'settings'][u'index.number_of_replicas']
-                print '\t\t\t Shards:', data_result[u'metadata'][u'indices'][index][u'settings'][u'index.number_of_shards']
+                print '\t\t\t State:', data_result_state[u'metadata'][u'indices'][index][u'state']
+                print '\t\t\t Replicas:', data_result_state[u'metadata'][u'indices'][index][u'settings'][u'index.number_of_replicas']
+                print '\t\t\t Shards:', data_result_state[u'metadata'][u'indices'][index][u'settings'][u'index.number_of_shards']
+                if data_result_state[u'metadata'][u'indices'][index][u'state'] == 'close':
+                    print '\t\t\t Status: CLOSED'
+                else:
+                    print '\t\t\t Status:', data_result_health[index][u'status']
 
         print '\t Nodes:'
-        for node in data_result[u'nodes']:
+        for node in data_result_state[u'nodes']:
             print '\t\t Node:', node
             if extended:
-                print '\t\t\t Name:', data_result[u'nodes'][node][u'name']
-                print '\t\t\t Transport Address:', data_result[u'nodes'][node][u'transport_address']
+                print '\t\t\t Name:', data_result_state[u'nodes'][node][u'name']
+                print '\t\t\t Transport Address:', data_result_state[u'nodes'][node][u'transport_address']
 
 
 def cluster_shutdown(cluster, host, port):
