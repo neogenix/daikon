ce99d55c8ca4d497a463750f1bd9a5b6466088c4
Patrick <patrick@eefy.net>
2011-11-06 19:37:09 -0600

    Created setup.py, and moved stuff around in prep for a package. Should close #21

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.10'
+
+setup(name='daikon',
+        version=VERSION,
+        description='ElasticSearch CLI',
+        long_description=''' ''',
+        classifiers=[],
+        keywords='',
+        author='Patrick Ancillotti',
+        author_email='patrick@eefy.net',
+        url='http://www.github.com/neogenix/daikon',
+        license='',
+        packages=find_packages(
+            exclude=['ez_setup', 'examples', 'tests']
+            ),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=['requests', 'anyjson'],
+        entry_points =
+            {'console_scripts':
+                ['daikon = daikon.daikon:main']
+            }
+)
