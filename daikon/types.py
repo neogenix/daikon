b97538a324868e5741143bc65d9c7b3efc02abb4
Patrick <patrick@eefy.net>
2011-11-10 21:55:48 -0600

    Forgot to run a git add. Should update #28, and update #47

---
+++ daikon/types.py
@@ -0,0 +1,34 @@
+#
+#   Copyright [2011] [Patrick Ancillotti]
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+class DaikonError(Exception):
+    """ Base Exception Class """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "ERROR: Error - " + self.value
+
+
+class ConfigError(DaikonError):
+    """ Config Exception Class """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "ERROR: Configuration Error - " + self.value
