b2d7850cf071bd9664d9da0b8b70e23a1cf5ceac
Jason Kölker <jason@koelker.net>
2011-11-17 15:36:08 -0600

    add connection object to abstract out talking to ES

---
+++ daikon/connection.py
@@ -0,0 +1,52 @@
+import requests
+import anyjson as json
+import urlparse
+
+class Connection(object):
+    _state = None
+    _health = None
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.url = 'http://%s:%s' % (host, port)
+
+    def get(self, path, raise_for_status=True):
+        url = urlparse.urljoin(self.url, path)
+        req = requests.get(url)
+        if raise_for_status:
+            req.raise_for_status()
+        return req
+
+    def post(self, path, data=None, raise_for_status=True):
+        url = urlparse.urljoin(self.url, path)
+        req = requests.post(url, data=data)
+        if raise_for_status:
+            req.raise_for_status()
+        return req
+
+    def delete(self, path, raise_for_status=True):
+        url = urlparse.urljoin(self.url, path)
+        req = requests.delete(url)
+        if raise_for_status:
+            req.raise_for_status()
+        return req
+
+    @property
+    def health(self):
+        if self._health is not None:
+            return self._health
+
+        path = '/_cluster/health?level=indices'
+        health = json.loads(self.get(path).content)
+        self._health = health[u'indices']
+        return self._health
+
+    @property
+    def state(self):
+        if self._state is not None:
+            return self._state
+
+        path = '/_cluster/state'
+        state = json.loads(self.get(path).content)
+        self._state = state[u'metadata'][u'indices']
