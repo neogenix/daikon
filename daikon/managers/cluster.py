781283b5be80a85a753af3eb6ffe4ac5579895d4
Patrick Ancillotti <patrick@eefy.net>
2012-09-22 22:12:23 -0600

    Moved cluster.py to print_output from display.py

--- daikon/managers/cluster.py
+++ daikon/managers/cluster.py
@@ -69,43 +69,54 @@ class Cluster(object):
             master_node_state = r_state[u'nodes'][master_node]
 
             d.print_output('Information:', level=1)
-            d.print_output('Cluster Name: $s', r_state[u'cluster_name'],
+            d.print_output('Cluster Name: %s', r_state[u'cluster_name'],
                            level=2)
             d.print_output('Master Node: %s', r_state[u'master_node'],
                            level=2)
 
             if extended:
-                print '\t\t\t Name: %s' % (master_node_state[u'name'])
-                print '\t\t\t Transport Address: %s' % \
-                        (master_node_state[u'transport_address'])
+                d.print_output('Name: %s', master_node_state[u'name'],
+                               level=3)
+                d.print_output('Transport Address: %s',
+                               master_node_state[u'transport_address'],
+                               level=3)
 
-            print '\t Indices:'
+            d.print_output('Indices:', level=1)
             for index in r_state[u'metadata'][u'indices']:
-                print '\t\t Name: %s' % (index)
+                d.print_output('Name: %s', index, level=2)
+
                 if extended:
                     index_result = r_state[u'metadata'][u'indices'][index]
-                    print '\t\t\t State: %s' % (index_result[u'state'])
-                    print '\t\t\t Replicas: %s' % \
-                            (index_result[u'settings']
-                                    [u'index.number_of_replicas'])
-                    print '\t\t\t Shards: %s' % \
-                            (index_result[u'settings']
-                                    [u'index.number_of_shards'])
-                    if index_result[u'state'] == 'close':
-                        print '\t\t\t Status: CLOSED'
-                    else:
-                        print '\t\t\t Status: %s' % \
-                                (r_health[index][u'status'])
+                    d.print_output('State: %s',
+                                   index_result[u'state'],
+                                   level=3)
+                    d.print_output('Replicas: %s',
+                                   index_result[u'settings']
+                                        [u'index.number_of_replicas'],
+                                   level=3)
+                    d.print_output('Shards: %s',
+                                   index_result[u'settings']
+                                        [u'index.number_of_shards'],
+                                   level=3)
 
-            print '\t Nodes:'
+                    if index_result[u'state'] == 'close':
+                        d.print_output('Status: CLOSED', level=3)
+                    else:
+                        d.print_output('Status: %s',
+                                       r_health[index][u'status'],
+                                       level=3)
+
+            d.print_output('Nodes:', level=1)
             for node in r_state[u'nodes']:
-                print '\t\t Node: %s' % (node)
+                d.print_output('Node: %s', node, level=2)
                 if extended:
-                    print '\t\t\t Name: %s' % \
-                            (r_state[u'nodes'][node][u'name'])
-                    print '\t\t\t Transport Address: %s' % \
-                            (r_state[u'nodes'][node]
-                                    [u'transport_address'])
+                    d.print_output('Name: %s',
+                                   r_state[u'nodes'][node][u'name'],
+                                   level=3)
+                    d.print_output('Transport Address: %s',
+                                   r_state[u'nodes'][node]
+                                        [u'transport_address'],
+                                   level=3)
 
         except (requests.RequestException, urllib2.HTTPError), e:
             msg = 'Error Fetching Cluster Status - %s' % (e)
@@ -116,7 +127,7 @@ class Cluster(object):
             request_url = 'http://%s:%s/_shutdown' % (host, port)
             request = requests.post(request_url)
             request.raise_for_status()
-            print 'SUCCESS: Shutting Down Cluster : "%s"' % (cluster)
+            d.print_output('SUCCESS: Shutting Down Cluster : "%s"', cluster)
         except (requests.RequestException, urllib2.HTTPError), e:
             msg = 'Shutting Down Cluster - %s' % (e)
             raise exceptions.ActionClusterError(msg)
